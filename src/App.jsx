0e4e391a80d983e1dfa2086ef25d7f5db14b96e9
Kamran <123491415+KAMRANANSARI01@users.noreply.github.com>
2023-08-12 00:30:13 +0530

    complete all file

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import CreateRouter from './CreateRouter/CreateRouter'
+
+function App() {
+
+  return (
+    <>
+          <div className="mx-auto mt-[20px] flex justify-center items-center "><img className="bg-none h-[150px] w-[300px] lg:h-[150px] lg:w-[470px] rounded  " src="./src/assets/img.png" alt="logo" /></div>
+         <CreateRouter/>
+     
+    </>
+  )
+}
+
+export default App
